c7067512aacc0d387ca086ddd0337351bef2c8ba
宮本七々子 <naaaaaaa.tooo@gmail.com>
2020-03-13 08:49:32 +0900

    20200313

--- at_coder/case_158/count_balls.py
+++ at_coder/case_158/count_balls.py
@@ -1,27 +1,6 @@
-import sys
+n,a,b = map(int,input().split())
 
-n,a,b=map(int,input().split())
-
-if a == 0:
-    print(0)
-    sys.exit()
-
-lst = []
-is_blue = True
-count = 0
-for i in range(n):
-    if is_blue:
-        lst.append('b')
-        count += 1
-        if count == a:
-            is_blue = False
-            count = 0
-    else:
-        lst.append('r')
-        count += 1
-        if count == b:
-            is_blue = True
-            count = 0
-
-result = list(filter(lambda x:x == 'b', lst))
-print(len(result))
+ans = n // (a + b) * a
+rem = n % (a + b)
+ans += min(rem, a)
+print(ans)
